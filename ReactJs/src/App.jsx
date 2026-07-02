6b7fa6991f943f8db360529bb85395af2986b74e
Azizkhan42 <azizkhan43025@gmail.com>
2026-07-02 13:55:34 +0500

    React props studied | props objects | destructuring props

--- ReactJs/src/App.jsx
+++ ReactJs/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div>
       <h1 className="font-bold pt-4 text-2xl">ReactJs with TailwindCSS</h1>
+
+      <Hello name="Aziz"></Hello>
+      <Hello name="Ahmad"></Hello>
+      <Hello name="Waqas"></Hello>
     </div>
   );
 }
