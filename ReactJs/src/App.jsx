ded5099960ed9bfdda223b3ef9c3008ff4a2e3bc
Azizkhan42 <azizkhan43025@gmail.com>
2026-07-02 14:39:59 +0500

    Deep practice on props like passing string, numbers, arrays, functions, objects

--- ReactJs/src/App.jsx
+++ ReactJs/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import Hello from './components/Hello'
+import Button from "./components/Button";
 
 function App() {
+  function Message(){
+    alert("Hello, this is alert using props function")
+  }
+  function data(){
+    alert("Data has been submitted successfully")
+  }
+  const userData ={
+    name: "Aziz",
+    age: 22,
+    isStudent: true
+  }
   return (
     <div>
       <h1 className="font-bold pt-4 text-2xl">ReactJs with TailwindCSS</h1>
@@ -9,6 +21,9 @@ function App() {
       <Hello name="Aziz"></Hello>
       <Hello name="Ahmad"></Hello>
       <Hello name="Waqas"></Hello>
+
+      <Button Message={Message} label="Click me"></Button>
+      <Button Message={data} label="Enter data" userData={userData}></Button>
     </div>
   );
 }
