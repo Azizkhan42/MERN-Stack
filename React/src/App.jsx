bed14b7eb573dca7f7fb832ad837f5378e6c475d
Azizkhan42 <azizkhan43025@gmail.com>
2026-08-20 23:16:53 +0500

    files udpated

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,19 +1,8 @@
-import Navbar from "./components/layout/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import { Routes, Route } from "react-router-dom";
-
 const App = () => {
   return (
     <div>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my React app!</p>
     </div>
   );
 };
